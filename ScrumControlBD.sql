3300345f9c66568c947034ca33c0a9533a8fa2f8
nilarrus <nilarrus69@gmail.com>
2018-12-03 19:40:45 +0100

    BBDD con usuarios Doomies

---
+++ ScrumControlBD.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Temps de generació: 03-12-2018 a les 19:39:27
+-- Versió del servidor: 5.7.24-0ubuntu0.18.04.1
+-- Versió de PHP: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de dades: `ScrumControlBD`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `Especificaciones`
+--
+
+CREATE TABLE `Especificaciones` (
+  `ID` int(11) NOT NULL,
+  `Num_Tarea` int(11) NOT NULL,
+  `Tarea` varchar(50) NOT NULL,
+  `Descripcion_Tarea` varchar(200) NOT NULL,
+  `Horas` int(11) NOT NULL,
+  `Dificultad` varchar(1) NOT NULL,
+  `Num_Sprint` int(11) NOT NULL,
+  `Estado` varchar(20) NOT NULL,
+  `ID_Dev_Asignado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `Grupos`
+--
+
+CREATE TABLE `Grupos` (
+  `ID` int(11) NOT NULL,
+  `Nombre_Grupo` varchar(50) NOT NULL,
+  `ID_Proyecto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `Proyectos`
+--
+
+CREATE TABLE `Proyectos` (
+  `ID` int(11) NOT NULL,
+  `Nombre_Proyecto` varchar(50) NOT NULL,
+  `Numero_Sprint` int(11) NOT NULL,
+  `ID_Scrum_Master` int(11) NOT NULL,
+  `ID_Product_Owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `Sprint`
+--
+
+CREATE TABLE `Sprint` (
+  `ID` int(11) NOT NULL,
+  `ID_Proyecto` int(11) NOT NULL,
+  `Numero_Sprint` int(11) NOT NULL,
+  `Horas_Maximas` int(11) NOT NULL,
+  `Fecha_entrega` date NOT NULL,
+  `Estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de la taula `Usuarios`
+--
+
+CREATE TABLE `Usuarios` (
+  `ID` int(11) NOT NULL,
+  `Nom` varchar(50) NOT NULL,
+  `Pasword` varchar(512) NOT NULL,
+  `Permisos` int(11) NOT NULL,
+  `ID_Grupo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Bolcant dades de la taula `Usuarios`
+--
+
+INSERT INTO `Usuarios` (`ID`, `Nom`, `Pasword`, `Permisos`, `ID_Grupo`) VALUES
+(1, 'Jose', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 0, NULL),
+(2, 'nil', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 0, NULL),
+(3, 'leandro', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 2, NULL),
+(4, 'enric', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 1, NULL);
+
+--
+-- Indexos per taules bolcades
+--
+
+--
+-- Index de la taula `Especificaciones`
+--
+ALTER TABLE `Especificaciones`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Dev_ID` (`ID_Dev_Asignado`);
+
+--
+-- Index de la taula `Grupos`
+--
+ALTER TABLE `Grupos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Proyecto_ID` (`ID_Proyecto`);
+
+--
+-- Index de la taula `Proyectos`
+--
+ALTER TABLE `Proyectos`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Scrum_Master_ID` (`ID_Scrum_Master`),
+  ADD KEY `FK_Product_Owner_ID` (`ID_Product_Owner`),
+  ADD KEY `FK_Numero_Sprint_ID` (`Numero_Sprint`);
+
+--
+-- Index de la taula `Sprint`
+--
+ALTER TABLE `Sprint`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index de la taula `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Nom` (`Nom`),
+  ADD KEY `FK_Grupo_ID` (`ID_Grupo`);
+
+--
+-- Restriccions per taules bolcades
+--
+
+--
+-- Restriccions per la taula `Especificaciones`
+--
+ALTER TABLE `Especificaciones`
+  ADD CONSTRAINT `FK_Dev_ID` FOREIGN KEY (`ID_Dev_Asignado`) REFERENCES `Usuarios` (`ID`);
+
+--
+-- Restriccions per la taula `Grupos`
+--
+ALTER TABLE `Grupos`
+  ADD CONSTRAINT `FK_Proyecto_ID` FOREIGN KEY (`ID_Proyecto`) REFERENCES `Proyectos` (`ID`);
+
+--
+-- Restriccions per la taula `Proyectos`
+--
+ALTER TABLE `Proyectos`
+  ADD CONSTRAINT `FK_Numero_Sprint_ID` FOREIGN KEY (`Numero_Sprint`) REFERENCES `Sprint` (`ID`),
+  ADD CONSTRAINT `FK_Product_Owner_ID` FOREIGN KEY (`ID_Product_Owner`) REFERENCES `Usuarios` (`ID`),
+  ADD CONSTRAINT `FK_Scrum_Master_ID` FOREIGN KEY (`ID_Scrum_Master`) REFERENCES `Usuarios` (`ID`);
+
+--
+-- Restriccions per la taula `Usuarios`
+--
+ALTER TABLE `Usuarios`
+  ADD CONSTRAINT `FK_Grupo_ID` FOREIGN KEY (`ID_Grupo`) REFERENCES `Grupos` (`ID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
