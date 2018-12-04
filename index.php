9254a4a84be760a97aace7cc5eb81c0fc13c2ea6
nilarrus <nilarrus69@gmail.com>
2018-12-04 16:53:54 +0100

    if inutil

--- index.php
+++ index.php
@@ -7,19 +7,15 @@
     <body>
     <?php
         echo "<h3>LOG IN</h3>";
-        echo "<form action='' method='post'>";
+        echo "<form action='proyectos' method='post'>";
             echo "User name: <input type='text' name='user' value='' placeholder='username'><br>";
             echo "Password: <input type='password' name='password' value='' placeholder='password'><br>";
             echo "<input type='submit' name='submit'>";
         echo "</form>";
-
-
         if ($_POST != null) {
             # Conexión a la bdd
             $conn = mysqli_connect('localhost','boss','1234');
             mysqli_select_db($conn, 'ScrumControlBD');
-
-            if(isset($_REQUEST['submit']))
 
             $user = $_POST['user'];
             $password = $_POST['password'];
