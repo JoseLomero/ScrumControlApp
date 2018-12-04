e1e022beca16e216f48f40ee007160d6a0887945
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-12-04 16:32:24 +0100

    Añadido el login

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+    <?php
+        echo "<h3>LOG IN</h3>";
+        echo "<form action='' method='post'>";
+            echo "User name: <input type='text' name='user' value='' placeholder='username'><br>";
+            echo "Password: <input type='password' name='password' value='' placeholder='password'><br>";
+            echo "<input type='submit' name='submit'>";
+        echo "</form>";
+
+        
+        if ($_POST != null) {
+            # Conexión a la bdd
+            $conn = mysqli_connect('localhost','jose','jose123');
+            mysqli_select_db($conn, 'ScrumControlBD');
+
+            if(isset($_REQUEST['submit']))
+
+            $user = $_POST['user'];
+            $password = $_POST['password'];
+
+            $consulta = "SELECT * FROM Usuarios WHERE Nom='".$user."' AND Pasword=SHA2('".$password."',512);";
+            echo "$consulta<br>";
+            
+            $resultat = mysqli_query($conn, $consulta);
+            if (!$resultat) {
+                $message  = 'Consulta invàlida: ' . mysqli_error($conn) . "\n";
+                $message .= 'Consulta realitzada: ' . $consulta;
+                die($message);
+            }
+
+            while( $registre = mysqli_fetch_assoc($resultat) ) {
+                echo "<p class='azul'> Hello ".$registre['user']."</p>";
+            }
+        }
+    ?>
+    </body>
+</html>
