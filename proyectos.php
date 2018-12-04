651726cecba706369244ef36d0ba2a8f7c3fecc8
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-12-04 17:27:06 +0100

    Añadido Nav con el usuario y boton de logout

--- proyectos.php
+++ proyectos.php
@@ -3,9 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>Proyectos Actuales</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <?php echo "Proyectos actuales"; ?>
+    <?php
+    $user = $_POST['user'];
+    echo "<nav>";
+    echo "  <p>".$user."</p>";
+    echo "  <a href='index.php'>";
+    echo "    <button><img class='logout' src='img/logout.png'></button>";
+    echo "  </a>";
+    echo "</nav>";
+    ?>
   </body>
 </html>
