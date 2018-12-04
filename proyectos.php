06a174037d1da7879971bd202e6421366f7b85c1
nilarrus <nilarrus69@gmail.com>
2018-12-04 16:47:37 +0100

    proyectos

---
+++ proyectos.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Proyectos Actuales</title>
+  </head>
+  <body>
+<?php echo "Proyectos actuales"; ?>
+  </body>
+</html>
