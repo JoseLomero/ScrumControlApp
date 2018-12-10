89d3852fab4da6a9f46d6944fb9584f1b9d0aa83
nilarrus <nilarrus69@gmail.com>
2018-12-10 19:28:20 +0100

    sessions andthings

--- proyectos.php
+++ proyectos.php
@@ -11,10 +11,11 @@
 
     echo "<nav>";
     echo "  <div class='nav-user'>";
-    echo "    <p class='usuario-user'>Bienvenido, ".$login_session."</p>";
+    echo "    <div class='app-Name' ><p>Scrum Control App</p></div>";
+    echo "    <div class='usuario-user'><p>Bienvenido, ".$login_session."</p>";
     echo "    <a href='logout.php'>";
     echo "      <button><img class='logout' src='img/logout.png'></button>";
-    echo "    </a>";
+    echo "    </a></div>";
     echo "  </div>";
     echo "</nav>";
     ?>
--- session.php
+++ session.php
@@ -5,7 +5,7 @@ session_start();
 $user_check = $_SESSION['login_user'];
 $ses_sql = mysqli_query($db,"SELECT Nom FROM Usuarios WHERE Nom='$user_check';");
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-$login_session = $row['username'];
+$login_session = $row['Nom'];
 if(!isset($_SESSION['login_user'])){
   header("location:index.php");
 }
