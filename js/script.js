8accabbd816c2e60630956273feb2bcaa9f539fa
josep <jsort45@gmail.com>
2018-12-13 19:25:09 +0100

    script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,77 @@
-function saltaError() {
-    botonError = document.getElementById("errores");
-    botonError.style.visibility = 'visible';
+
+newProyect(tipoUsuario);
+
+
+
+function newProyect(user){
+	if(user == 2){
+		var parent = document.querySelector(".proyect-table");
+		addElement(parent,"button","Add Proyect",["onclick=showForm();","name=addProyect"])
+	}
 }
 
-function quitaError() {
-    botonError = document.getElementById("errores");
-    botonError.style.visibility = 'hidden';
+/*     ------------------------------------------   */
+
+function showForm(){
+	document.querySelector("[name='addProyect']").disabled = true;
+	document.querySelector(".new-proyect-box").style.display = "block";
+	var parent = document.querySelector(".new-proyect-box");
+	var form = addElement(parent,"form",undefined,["action=insert.php","method=post","id=createProyect"]);
+	var div = addElement(form,"div",undefined,["class=card-content container"]);
+	addElement(div,"span","Añadir nuevo proyecto",["class=card-title"]);
+	var divrow = addElement(div,"div",undefined,["class=row"]);
+	
+	createText(divrow,"Nombre del proyecto: ","ProyectName");
+	createText(divrow,"Descripción del proyecto: ","descripcion");
+
+	createSelect(divrow,"Scrum Master del proyecto: ","scrumMaster");
+	createSelect(divrow,"Product Owner del proyecto: ","productOwner");
+	createGroup(divrow,"Grupos del proyecto: ","grupos");
+
+	addElement(form,"div","Crear",["class=button","onclick=checkNulls()"]);
+}
+
+function createText(parent,labelText,name){
+	var divcol = addElement(parent, "div", undefined, ["class=input-field col s12"]);
+	addElement(divcol,"label",labelText,["for="+name]);
+	addElement(divcol,"input",undefined,["type=text","name="+name]);
+}
+
+function createSelect(parent,labelText,name){
+	var divcol = addElement(parent, "div", undefined, ["class=col s12"]);
+	addElement(divcol,"label",labelText,undefined);
+	addElement(divcol,"br",labelText,undefined);
+	var select = addElement(divcol,"select",undefined,["name="+name]);
+	addElement(select,"option",undefined,["selected=selected","disabled=true","value="]).text = "Selecciona una opción";
+	createDropDown(select,name);
+	initSelect();
+}
+
+function createDropDown(select,tipoUsuario){
+	personas.forEach(function(element){
+		if(element.tipo == tipoUsuario){
+			var option = addElement(select,"option");
+			option.text = element.name;
+			option.value = element.id;
+			select.add(option);
+		}
+	});
+}
+
+function createGroup(parent,labelText,name){
+	var divcol = addElement(parent, "div", undefined, ["class=col s12"]);
+	addElement(divcol,"label",labelText,undefined);
+	addElement(divcol,"br",labelText,undefined);
+	var select = addElement(divcol,"select",undefined,["name="+name+"[]","multiple=true"]);
+	addElement(select,"option",undefined,["disabled=true","value="]).text = "Selecciona una opción";
+	insertGroup(select,name);
+	initSelect();
+}
+function insertGroup(select){
+	grupos.forEach(function(element){
+		var option = addElement(select,"option");
+		option.text = element.name;
+		option.value = element.id;
+		select.add(option);
+	});
 }
