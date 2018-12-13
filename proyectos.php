5ecd3e58fde2c56715170465d0baff07b3acea10
josep <jsort45@gmail.com>
2018-12-13 16:24:36 +0100

    hihihoho

--- proyectos.php
+++ proyectos.php
@@ -10,6 +10,7 @@
   <body>
     <?php
     //proyectos:
+    //hola
     $sql = "SELECT Nombre_Proyecto, Descripcion FROM Proyectos;";
     mysqli_select_db($db,'ScrumControlBD');
     $resultat = mysqli_query($db,$sql);
@@ -22,18 +23,16 @@
           <button><img class='logout' src='img/logout.png'></button>
         </a></div>
       </div>
-    </nav>
-    <div id='errores'>
-      <div class='errores-icono'><img src='img/exclamation.png' class='basicError-icon'></div>
-      <div class='errores-mensaje'><p>Error!</p></div>
-    </div>
-    <button onclick='saltaError()'>Crea errores!</button>
-    <button onclick='quitaError()'>Resuelve errores!</button>";
-    echo "<div>";
+    </nav>";
+    echo "<div class='proyect-list'>
+      <div class='proyect-title'>Proyectos</div>
+      <div class='proyect-table'>
+        <ul>";
     while ($registre = mysqli_fetch_assoc($resultat)) {
-      echo "<div><h2>".$registre['Nombre_Proyecto']."</h2></div>";
-      echo "<div><h4>".$registre['Descripcion']."</h4></div>";
+      echo "<li><a href='#' name='proyecto'>".$registre['Nombre_Proyecto']." (".$registre['Descripcion'].")</a></li>";
     }
+      echo "</ul>";
+    echo "</div>";
     echo "</div>";
     ?>
   </body>
